7a681e32bdc8c354ce8892187b9acc36c14d9705
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 14:13:31 -0300

    Update TeoremaPitagoras.py

--- Teorema de Pitágoras/TeoremaPitagoras.py
+++ Teorema de Pitágoras/TeoremaPitagoras.py
@@ -7,7 +7,7 @@ import math
 y = float(input("Digite a distância de Y: "))
 x = float(input("Digite o valor da distância de X: "))
 
-# Calculo e declaração da variável "resultado", que representa o resultado do teorema de Pitágoras.
+# Cálculo e declaração da variável "resultado", que representa o resultado do teorema de Pitágoras.
 resultado = math.sqrt((math.pow(y,2) + math.pow(x,2)))
 
 # Imprimindo a variável "resultado".
