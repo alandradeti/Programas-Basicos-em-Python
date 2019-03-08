549cf0de7056c01b51d97d0227c3fb3150aaaf70
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 13:47:29 -0300

    Add files via upload

---
+++ Produto Notável com Math/MathProdutoNotavel.py
@@ -0,0 +1,14 @@
+# Importando biblioteca Math
+import math
+
+# Leitura e declaração das variáveis:
+# "a", que representa o valor de A;
+# "b", que representa o valor de B.
+a = int(input("Digite o valor de A: "))
+b = int(input("Digite o valor de B: "))
+
+# Calculo e declaração da variável "valorFinal", que representa o valor final.
+valorFinal = (math.pow(a,2) + 2*(a*b) + math.pow(b,2))
+
+# Imprimir as variáveis "a" e "b" e  "valorFinal".
+print("O valor de (a+b)²: ", valorFinal)
---
+++ Teorema de Pitágoras/TeoremaPitagoras.py
@@ -0,0 +1,14 @@
+# Importando biblioteca Math
+import math
+
+# Leitura e declaração das variáveis:
+# "a", que representa o valor de y;
+# "b", que representa o valor de x.
+y = float(input("Digite a distância de Y: "))
+x = float(input("Digite o valor da distância de X: "))
+
+# Calculo e declaração da variável "resultado", que representa o resultado do teorema de Pitágoras.
+resultado = math.sqrt((math.pow(y,2) + math.pow(x,2)))
+
+# Imprimindo a variável "resultado".
+print("O resultado é: ", resultado)
