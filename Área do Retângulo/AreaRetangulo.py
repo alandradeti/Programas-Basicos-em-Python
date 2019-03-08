cc088f29f114d572a74f11a94921cc423176c356
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 14:22:06 -0300

    Add files via upload

---
+++ Área do Retângulo/AreaRetangulo.py
@@ -0,0 +1,11 @@
+# Leitura e declaração das variáveis:
+# "base", que representa a base do retângulo;
+# "altura", que representa a altura do retângulo.
+base = float(input("Digite o tamanho do primeiro lado? "))
+altura = float(input("Digite o tamanho do segundo lado? "))
+
+# Cálculo e declaração da variável "area", que representa a area do retângulo.
+area = (base * altura)
+
+# Imprimir a variável "area".
+print("A área do retângulo é: ", area)
