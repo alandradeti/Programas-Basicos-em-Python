2f95bb70223324476175ea022283003f19384ce7
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 14:10:24 -0300

    Update ConverteTemperatura.py

--- Converter Temperatura/ConverteTemperatura.py
+++ Converter Temperatura/ConverteTemperatura.py
@@ -1,7 +1,8 @@
 # Leitura e declaração das variáveis:
-# "fahrenheit", que representa a temperatura em Fahrenheit;
-# "celsius", que representa a temperatura em Celsius.
+# "fahrenheit", que representa a temperatura em Fahrenheit.
 fahrenheit = float(input("Digite o grau em Fahrenheit: "))
+
+# Cálculo e declaração da variável "celsius", que representa a temperatura em Celsius.
 celsius = ((fahrenheit - 32)/9) * 5
 
 # Imprimir a variável "celsius".
