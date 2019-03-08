cb7f633c15b3baa3c6cba13647af5db0835cbc41
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 14:11:19 -0300

    Update CalcularSalario.py

--- Cálcular Salário/CalcularSalario.py
+++ Cálcular Salário/CalcularSalario.py
@@ -6,13 +6,13 @@ horasTrabalhadas = int(input("Digite as horas trabalhadas no mês: "))
 valorHoras = float(input("Digite o valor da hora trabalhada: "))
 percentualDesconto = float(input("Digite o percentual de desconto: "))
 
-# Calculo e declaração da variável "salarioBruto", que representa o salário bruto.
+# Cálculo e declaração da variável "salarioBruto", que representa o salário bruto.
 salarioBruto = horasTrabalhadas*valorHoras
 
-# Calculo e declaração da variável "totalDesconto", que representa o total de desconto.
+# Cálculo e declaração da variável "totalDesconto", que representa o total de desconto.
 totalDesconto = (percentualDesconto/100)*salarioBruto
 
-# Calculo e declaração da variável "salarioLiquido", que representa o salário líquido.
+# Cálculo e declaração da variável "salarioLiquido", que representa o salário líquido.
 salarioLiquido = salarioBruto-totalDesconto
 
 # Imprimir as váriaveis "horasTrabalhadas", "salarioBruto", "totalDesconto" e "salarioLiquido"
