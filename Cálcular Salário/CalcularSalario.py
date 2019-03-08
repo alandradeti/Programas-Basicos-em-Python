3f136947f5ef614f050a43c2d578cb8083f29f95
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 20:55:32 -0300

    Update CalcularSalario.py

--- Cálcular Salário/CalcularSalario.py
+++ Cálcular Salário/CalcularSalario.py
@@ -1,3 +1,4 @@
+# Autor: Ailton Lima de Andrade.
 # Leitura e declaração das variáveis:
 # "horasTrabalhadas", que representa as horas trabalhadas;
 # "valorHoras", que representa o valor das horas trabalhadas;
