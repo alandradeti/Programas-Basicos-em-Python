433c02c97e415339409392a9c42a6c92a774bc7d
ailtonlima510 <47827376+ailtonlima510@users.noreply.github.com>
2019-03-08 21:34:51 -0300

    Add files via upload

---
+++ Número Comparação/NumeroComparacao.py
@@ -0,0 +1,9 @@
+# Leitura e declaração das variáveis:
+# "a", que representa o valor de "a";
+# "b", que representa o valor de "b".
+a = 10
+b = 5
+
+# Imprimir a comparação dos valores das variáveis de "a" e "b".
+# Leitura (não é verdade que "a" é igual a "b")
+print("não(A==B)? ", not(a==b))
---
+++ Número Positivo/NumeroPositivo.py
@@ -0,0 +1,9 @@
+# Autor: Ailton Lima de Andrade.
+
+# Leitura e declaração das variáveis:
+# "numero", que representa um número digitado pelo usuário.
+numero = int(input("Digite um número inteiro: "))
+
+# Verificar se a variável "numero" é maior que 0.
+if numero>0:
+    print("O número que você digitou é positivo!")
